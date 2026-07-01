7fa1025c2c55a3ce1cf4863c1f81f826f0c51bbd
alynch8888 <122303285+alynch8888@users.noreply.github.com>
2026-07-01 15:03:13 -0500

    Google Colab VSCode file Update 07/01/26
    File updates

--- AL_BField_Analysis/Comp_Mine_Vs_CVMFS/Find_Bad_BDiff_Vals.py
+++ AL_BField_Analysis/Comp_Mine_Vs_CVMFS/Find_Bad_BDiff_Vals.py
@@ -14,7 +14,7 @@ from scipy.optimize import curve_fit
 from datetime import date
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from Comp_Mine_Vs_CVMFS.Show_BF_Diff_and_Err import frac_error, cvmfs_coord
+from Comp_Mine_Vs_CVMFS.Show_BF_Diff_and_Err import frac_error, cvmfs_coord, mydata_field, cvmfs_field
 #Useful names that can be used elsewhere
 today = date.today()
 
@@ -22,8 +22,8 @@ today = date.today()
 print("Running "+os.path.splitext(os.path.basename(__file__))[0]+"...\n") #This prints out the name of the script and the fact that it is running. This is done to show which file is running when it is called by another script.
 
 field_error = [frac_error[:, 0], frac_error[:, 1], frac_error[:, 2]]
-sep = "\n--------------------------------------------------\n"
-
+sep = "--------------------------------------------------"
+sep_n = "\n--------------------------------------------------\n"
 # print(field_error,sep)
 # print(field_error[2],sep)
 # print(field_error[2]>1,sep)
@@ -32,12 +32,26 @@ mask1 = (field_error[2] > 1) | (field_error[2] < -1)
 
 # # Equivalent shorthand using absolute value
 # mask1 = np.abs(field_error[2]) > 1
-print(sep)
-mask1 = field_error[2] > 1
+print(sep_n)
+fe_bound = input("What field error bound do you want to look at? ")
+print(f"You chose: {fe_bound}")
+mask1 = field_error[2] > float(fe_bound)
+# print("\n # of FracErrs |B|>1: ",len(mask1))
 
 if mask1.any():
     coords = cvmfs_coord[mask1]
     errors = field_error[2][mask1]
-    for (x, y, z), err in zip(coords, errors):
-        print(f"X: {x:.2f}  Y: {y:.2f}  Z: {z:.2f}  frac_error_Bz: {err:.6f}")
-
+    # mydata_field = mydata_field[2][mask1]
+    # cvmfs_field = cvmfs_field[2][mask1]
+    true_count = 0
+    test = 0
+    for (x, y, z), err, (bx0,by0,bz0),(bx1,by1,bz1) in zip(coords, errors, mydata_field, cvmfs_field):
+        print(f"MyData Field: Bx: {bx0:.3f} G, By: {by0:.3f} G, Bz: {bz0:.3f} G")
+        print(f"CVFMS Field: Bx: {bx1:.3f} G, By: {by1:.3f} G, Bz: {bz1:.3f} G")
+        print(f"X: {x+3896:.2f}mm  Y: {y:.2f}mm  Z: {z:.2f}mm | frac_error_Bz: {err:.4f}") 
+        print(sep+"\n")
+        true_count = true_count + 1
+        test += 1
+    print(sep,sep,"\n# of |B| FracErrs > {fe_bound}: ",true_count)
+    # print(sep, "\n# of |B| FracErrs > 1: ",test)
+    # print(sep, "\n# of |B| FracErrs > 1: ",mask1.sum())
